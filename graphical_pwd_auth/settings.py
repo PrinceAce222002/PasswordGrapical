3b7b9d6b3e436830f41621ebb83e8a344b0f8372
princeAD <prahanjaladhikariuk@gmail.com>
2022-07-08 17:49:44 +0530

    hello

--- graphical_pwd_auth/settings.py
+++ graphical_pwd_auth/settings.py
@@ -143,4 +143,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'aditibhakuni1005@gmail.com'
-EMAIL_HOST_PASSWORD = 'vgkfontqmpdjzvsr'
+EMAIL_HOST_PASSWORD = 'faagaksaccpvemel'
